870fdb7d444d2b40e9b0586cd88a09102c6563e4
Viveklagishetty <vivekdattalagishetty@gmail.com>
2026-08-03 16:50:56 +0530

    docs: add project demonstration video

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ def get_db_connection():
             host=os.getenv("MYSQL_HOST", "localhost"),
             port=int(os.getenv("MYSQL_PORT", "3306")),
             user=os.getenv("MYSQL_USER", "root"),
-            password=os.getenv("MYSQL_PASSWORD", "password"),
+            password=os.getenv("MYSQL_PASSWORD", "Vivek@12"),
             database=os.getenv("MYSQL_DATABASE", "library_ms"),
             autocommit=True,
         )
